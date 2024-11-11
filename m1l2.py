fc914e10c200b7519028eb0190746bfc9bf3f1d2
Mythos96 <107173322+Mythos96@users.noreply.github.com>
2024-11-11 20:28:53 +0300

    Update m1l2.py
    Programa hoş geldiniz yazısı eklendi
    Karakterler listesine yeni semboller eklendi
    Oluşturulan şifrenin aynı karakterleri içermemesi için geliştirmeler yapıldı

--- m1l2.py
+++ m1l2.py
@@ -1,11 +1,18 @@
 import random
 
-karakterler = "+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+print("Rastgele güçlü şifre oluşturma programına hoş geldiniz!")
+karakterler = [".","}","{","é",",",";","^",":","~","'","_","]","[","(",")","+","-","/","*","!","&","$","#","?","=","@","<",">","a","b","c","d","e","f","g","h","i","j","k","l","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0"]
 uzunluk = int(input("Şifre uzunluğu kaç karakter olsun?: "))
 
 sifre = ""
 
 for i in range(uzunluk):
-    sifre += random.choice(karakterler)
+    rastgele_karakter = random.choice(karakterler)
+    if rastgele_karakter in sifre:
+        karakterler.remove(rastgele_karakter)
+        sifre += random.choice(karakterler)
 
+    else:
+        sifre += rastgele_karakter
+    
 print(sifre)
