9219cdbd11ea29322bd7bd4b92277870e5e69e56
Mythos96 <107173322+Mythos96@users.noreply.github.com>
2024-11-12 12:17:21 +0300

    Update m1l2.py
    Uzunluk değişkenine string türünde ifadeler girildiğinde program artık çalışıyor.

--- m1l2.py
+++ m1l2.py
@@ -2,7 +2,11 @@ import random
 
 print("Rastgele güçlü şifre oluşturma programına hoş geldiniz!")
 karakterler = [".","}","{","é",",",";","^",":","~","'","_","]","[","(",")","+","-","/","*","!","&","$","#","?","=","@","<",">","a","b","c","d","e","f","g","h","i","j","k","l","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0"]
-uzunluk = int(input("Şifre uzunluğu kaç karakter olsun?: "))
+uzunluk = input("Şifre uzunluğu kaç karakter olsun?: ")
+
+if isinstance(uzunluk,str):
+    uzunluk = len(uzunluk)
+    print("Şifreniz girdiğiniz kelimenin uzunluğunda yazdırılacaktır")
 
 sifre = ""
 
