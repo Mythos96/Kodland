5f288c11c595edd3c603e868f2395f9bbbb0c7ee
Mythos96 <107173322+Mythos96@users.noreply.github.com>
2024-11-25 23:48:14 +0300

    Update m1l4.py

--- m1l4.py
+++ m1l4.py
@@ -1,5 +1,6 @@
 import discord
-from Token import token
+from bilgi import *
+from ayarlar import *
 from discord.ext import commands
 
 intents = discord.Intents.default()
@@ -7,9 +8,14 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+bot.remove_command('help')
+
 @bot.event
 async def on_ready():
     print(f'{bot.user} olarak giriş yaptık')
+
+    activity = discord.Game("!help")
+    await bot.change_presence(status=discord.Status.online, activity=activity)
 
 @bot.command()
 async def merhaba(ctx):
@@ -21,12 +27,47 @@ async def bye(ctx):
 
 @bot.command()
 async def add(ctx, left: int, right: int):
-    await ctx.send(left + right)
+    await ctx.send(f"{left} + {right} = {left + right}")
 
-@bot.group()
-async def cool(ctx):
-    if ctx.invoked_subcommand is None:
-        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+@bot.command()
+async def şifre(ctx):
+    def sifre_olusturucu():
+        karakterler = [".","}","{","é",",",";","^",":","~","'","_","]","[","(",")","+","-","/","*","!","&","$","#","?","=","@","<",">","a","b","c","d","e","f","g","h","i","j","k","l","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0"]
+        sifre = ""
+        for i in range(8):
+            sifre += random.choice(karakterler)
+        return sifre
+    user = ctx.author
+    await user.send(f"Şifreniz: {sifre_olusturucu()}")
+    await ctx.send(f"{user.mention} Şifreniz DM ile göderildi!")
+
+@bot.command()
+async def coin(ctx):
+    await ctx.send(yazi_tura())
+
+@bot.command()
+async def ranmoji(ctx):
+    await ctx.send(emoji_olusturucu())
+
+@bot.command()
+async def shutdown(ctx):
+    if ctx.author.id == "id":
+        await ctx.send("{Logging off}")
+        await bot.close
+    else:
+        await ctx.send("Bu komutu kullanmak için yetkiniz yok!")
+
+@bot.command()
+async def ping(ctx):
+    if ctx.author.id == "id":
+        latency = round(bot.latency * 1000)
+        await ctx.send(f"pong! {latency}ms")
+    else:
+        await ctx.send("Bu komutu kullanmak için yetkiniz yok!")
+
+@bot.command()
+async def help(ctx):
+    await ctx.send("Myth Bot Komutları:\n- !merhaba\n- !bye\n- !şifre\n- !coin\n- !ranmoji \n- !add\n\nYetkili komutları:\n- !shutdown\n- !ping")
 
 
 bot.run(token)
